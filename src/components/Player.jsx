63990fdeb9fc20f0ca5451d29c242d8965d34f50
Prabhs-Kaur <prabhjotkaur1243@gmail.com>
2024-02-20 12:22:33 -0800

    Add Initial Files of TIc-Tac-Toe game

---
+++ src/components/Player.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+
+
+export default function Player({ initialName, symbol }) {
+  const [playerName, setPlayerName] = useState(initialName);
+
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handleEditClick() {
+    setIsEditing((editing) => !editing);
+  }
+
+  function handleChange(event) {
+    setPlayerName(event.target.value);
+  }
+
+  let editPlayerName = <span className="player-name">{playerName}</span>;
+  //let changeBtn='Edit';
+
+  if (isEditing) {
+    editPlayerName = (
+      <input type="text" required value={playerName} onChange={handleChange} />
+    );
+    //changeBtn='Save';
+  }
+
+  return (
+    <>
+      <li>
+        <span className="player">
+          {editPlayerName}
+          <span className="player-symbol">{symbol}</span>
+        </span>
+        <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
+      </li>
+    </>
+  );
+}
